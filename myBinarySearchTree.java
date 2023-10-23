12aa7fd0b7d837695da9e1b515939d858b5dc4a5
Hbeden4 <115094079+Hbeden4@users.noreply.github.com>
2023-10-23 14:57:28 -0400

    Add files via upload

---
+++ myBinarySearchTree.java
@@ -0,0 +1,33 @@
+
+public class myBinarySearchTree extends BinarySearchTree {
+
+    private int size;
+
+    public myBinarySearchTree() {
+        super();
+        size = 0;
+    }
+
+    
+    private void add(int node, Node<Integer> root) {
+    	
+    	
+    	
+    	
+    }
+    @Override
+    public boolean add(int value) {
+        
+    	add(value);
+    		
+    		size++;
+    		return true;
+   	
+    	}
+        
+    
+
+    public int getSize() {
+        return size;
+    }
+}
