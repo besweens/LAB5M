3c15d895b88a23e950fad655349bc733c2f0b854
besweens <144698652+besweens@users.noreply.github.com>
2023-10-23 14:49:28 -0400

    blank main class

--- LAB5M.java
+++ LAB5M.java
@@ -5,12 +5,25 @@
  * Subtask 1: Herve
  * Subtask 2: Zachary
  * Subtask 3: Audrey
+ *
+ * Create myBinarySearchTree, myTreeIterator, TreeIteratorTesting objects
+ * Pass the first two objects to the TreeIteratorTesting constructor
+ * Print the average and the sum values
  */
 
 import java.io.IOException;
 
 public class LAB5M {
     public static void main(String[] args) throws IOException {
+
+        myBinarySearchTree testTree = new myBinarySearchTree();
+        myTreeIterator testIter = new myTreeIterator();
+        TreeIteratorTesting testObj = new TreeIteratorTesting(myBinarySearchTree, myTreeIterator);
+
+        System.out.println("Lab 5M output\n");
+        System.out.println("Tree average: " + testObj.average());
+        System.out.println("Tree max: " + testObj.max());
+
 
     }
 }
