7b914a7710f66afae1e4dcabed3e68e7962376d9
Hbeden4 <115094079+Hbeden4@users.noreply.github.com>
2023-10-23 17:10:56 -0400

    Update myBinarySearchTree.java

--- myBinarySearchTree.java
+++ myBinarySearchTree.java
@@ -1,4 +1,3 @@
-
 public class myBinarySearchTree extends BinarySearchTree {
 
     private int size;
@@ -8,24 +7,11 @@ public class myBinarySearchTree extends BinarySearchTree {
         size = 0;
     }
 
-    
-    private void add(int node, Node<Integer> root) {
-    	
-    	
-    	
-    	
-    }
     @Override
-    public boolean add(int value) {
-        
-    	add(value);
-    		
-    		size++;
-    		return true;
-   	
-    	}
-        
-    
+    public void add(int value) {
+        super.add(value);
+        size++;
+    }
 
     public int getSize() {
         return size;
