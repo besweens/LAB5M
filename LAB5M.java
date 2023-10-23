e45f9394b2dccc104b8156da166f2f890bc25698
besweens <144698652+besweens@users.noreply.github.com>
2023-10-23 14:27:53 -0400

    blank main class

--- LAB5M.java
+++ LAB5M.java
@@ -1,11 +1,16 @@
 /**
  * LAB5M
  *
+ * Project Manager: Ben Sweeney
+ * Subtask 1: Herve
+ * Subtask 2: Zachary
+ * Subtask 3: Audrey
  */
 
 import java.io.IOException;
 
 public class LAB5M {
     public static void main(String[] args) throws IOException {
+
     }
 }
