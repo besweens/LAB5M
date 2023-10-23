37fdeb37e80a18399f379c18fdbcabba0ec2a9ce
Hbeden4 <115094079+Hbeden4@users.noreply.github.com>
2023-10-23 14:54:52 -0400

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,51 +1,36 @@
-public abstract class BinarySearchTree<Integer> {
-    /**
-     * Binary Tree class for Lab5M
-     * Utilizing nodes, binary trees can be built with a root and pointers for
-     * left and right children. Integer data is stored within the node.
-     *
-     * @author Sweeney
-     * @version 20231016
-     */
 
-    //root is the top level of our binary tree
-    protected Node<Integer> root;
+public class myBinarySearchTree extends BinarySearchTree {
 
-    private  int size;
+    private int size;
 
-    /**
-     *Constructor for BinaryTree objects
-     */
-    public BinarySearchTree(){
-        root = null;
+    public myBinarySearchTree() {
+        super();
         size = 0;
     }
 
-    public abstract boolean add(int data);
-    public int getSize() {return size;}
+    
+    private void add(int node, Node<Integer> root) {
+    	
+    	if(node==null) {
+    		
+    	}
+    	
+    	
+    	
+    }
+    @Override
+    public boolean add(int value) {
+        
+    	add(value);
+    		
+    		size++;
+    		return true;
+   	
+    	}
+        
+    
 
-    /**
-     * Internal class to build nodes for our binary tree.
-     * @param <Integer> nodes of type Integer
-     *
-     */
-    protected static class Node<Integer>{
-        Integer data;
-        Node<Integer> left;
-        Node<Integer> right;
-
-        /**
-         * Builder for nodes
-         * @param data
-         */
-        public Node(Integer data) {
-            this.data = data;
-            left = null;
-            right = null;
-        }
-
-
+    public int getSize() {
+        return size;
     }
 }
-
-
